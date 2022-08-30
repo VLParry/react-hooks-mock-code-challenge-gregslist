80a54315557f92a2003d90f6914ef92a3f41d8a2
Victoria Parry <vlparry@gmail.com>
2022-08-30 15:52:55 -0600

    commit tree ec466bf6c5af9bd53cf208597a3a722a2da71182

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 import React from "react";
+import { useState } from "react/cjs/react.development";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+const [search, setSearch] = useState("")
+  //app is the closest parent to listings container and search so we need to set our search state here for both of them to accerss it.
+
+  function handleSearch(newSearch){
+    setSearch(newSearch)
+  }
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header onSearch={handleSearch} />
+      <ListingsContainer search={search} />
     </div>
   );
 }
